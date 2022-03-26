b711c8eb69db09aa869e3f8923385fefe9c2e680
Ankur Chunekar <89077985+AnkurChunekar@users.noreply.github.com>
2022-03-26 17:47:47 +0530

    Refactor: remove unecessary anchor tags

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,75 +3,49 @@ import "./Footer.css";
 export function Footer() {
   return (
     <>
-    <footer className="footer dark">
-      <div className="footer-container">
-        <div className="row flex flex-wrap">
-          <div className="footer-col">
-            <h4>Company</h4>
-            <ul className="list-style-none">
-              <li>
-                <a >About Us</a>
-              </li>
-              <li>
-                <a >Our Services</a>
-              </li>
-              <li>
-                <a >FAQ</a>
-              </li>
-            </ul>
-          </div>
-          <div className="footer-col">
-            <h4>Categories</h4>
-            <ul className="list-style-none">
-              <li>
-                <a >Weights</a>
-              </li>
-              <li>
-                <a >Equipments</a>
-              </li>
-              <li>
-                <a >Accessories</a>
-              </li>
-              <li>
-                <a >Supplements</a>
-              </li>
-            </ul>
-          </div>
-          <div className="footer-col">
-            <h4>Our Shop</h4>
-            <ul className="list-style-none">
-              <li>
-                <a >Orders</a>
-              </li>
-              <li>
-                <a >Wishlist</a>
-              </li>
-              <li>
-                <a >Account</a>
-              </li>
-            </ul>
-          </div>
-          <div className="footer-col">
-            <h4>Follow Us</h4>
-            <div className="social-links">
-              <a >
+      <footer className="footer dark">
+        <div className="footer-container">
+          <div className="row flex flex-wrap">
+            <div className="footer-col">
+              <h4>Company</h4>
+              <ul className="list-style-none">
+                <li>About Us</li>
+                <li>Our Services</li>
+                <li>FAQ</li>
+              </ul>
+            </div>
+            <div className="footer-col">
+              <h4>Categories</h4>
+              <ul className="list-style-none">
+                <li>Weights</li>
+                <li>Equipments</li>
+                <li>Accessories</li>
+                <li>Supplements</li>
+              </ul>
+            </div>
+            <div className="footer-col">
+              <h4>Our Shop</h4>
+              <ul className="list-style-none">
+                <li>Orders</li>
+                <li>Wishlist</li>
+                <li>Account</li>
+              </ul>
+            </div>
+            <div className="footer-col">
+              <h4>Follow Us</h4>
+              <div className="social-links">
                 <i className="fa-brands fa-twitter" />
-              </a>
-              <a >
+
                 <i className="fa-brands fa-linkedin" />
-              </a>
-              <a >
+
                 <i className="fa-brands fa-github" />
-              </a>
-              <a >
+
                 <i className="fa-brands fa-hashnode" />
-              </a>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </footer>
-  </>
-  
+      </footer>
+    </>
   );
 }
