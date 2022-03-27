96c2f065b01073df99faf2f0aa0e7dd4f0a3a5a8
Ankur Chunekar <89077985+AnkurChunekar@users.noreply.github.com>
2022-03-27 18:21:35 +0530

    Feat: Add links to drawer component

--- src/components/drawer/Drawer.jsx
+++ src/components/drawer/Drawer.jsx
@@ -1,4 +1,5 @@
 import "./Drawer.css";
+import { Link } from "react-router-dom";
 
 export function Drawer(){
     return <div className="nav-drawer dark fs-5">
@@ -6,25 +7,35 @@ export function Drawer(){
             <span > <i className="fa-solid fa-house"></i> </span>
             <span> Home </span>
         </div>
-        <div className="drawer-item active">
+      <Link to="/" >
+        <div className="drawer-item">
             <span > <i className="fa-regular fa-compass"></i> </span>
             <span> Explore </span>
         </div>
+        </Link>
+        <Link to="/playlist" >
         <div className="drawer-item">
             <span > <i className="fa-solid fa-list-check"></i> </span>
             <span> Playlists </span>
         </div>
+        </Link>
+        <Link to="/watchlater" >
         <div className="drawer-item">
             <span > <i className="fa-regular fa-clock"></i> </span>
             <span> Watch Later </span>
         </div>
+        </Link>
+        <Link to="/liked" >
         <div className="drawer-item">
             <span > <i className="fa-solid fa-thumbs-up"></i> </span>
             <span> Liked </span>
         </div>
+        </Link>
+        <Link to="/history" >
         <div className="drawer-item">
             <span > <i className="fa-solid fa-clock-rotate-left"></i> </span>
             <span> History </span>
         </div>
+        </Link>
     </div>
 }
