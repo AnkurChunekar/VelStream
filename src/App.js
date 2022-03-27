b6410ad32d2aecd4878692c3daefc483ca96f2cd
Ankur Chunekar <89077985+AnkurChunekar@users.noreply.github.com>
2022-03-27 18:20:53 +0530

    Feat: install react router and define routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import "./App.css";
 import { Navbar } from "./components";
-import { PlaylistPage } from "./pages";
+import { History, Liked, PlaylistPage, VideoListing, WatchLater } from "./pages";
+import { Routes, Route } from "react-router-dom";
 
 
 function App() {
   return (
     <div className="App dark">
       <Navbar />
-      <PlaylistPage />
+      <Routes>
+        <Route path="/" element={<VideoListing />} />
+        <Route path="/playlist" element={<PlaylistPage />} />
+        <Route path="/watchlater" element={<WatchLater />} />
+        <Route path="/liked" element={<Liked />} />
+        <Route path="/history" element={<History />} />
+      </Routes>
     </div>
   );
 }
