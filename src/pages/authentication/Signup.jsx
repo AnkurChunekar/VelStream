0851dbb8ea6b3aa35f094206ea33f83c35e74a95
Ankur Chunekar <89077985+AnkurChunekar@users.noreply.github.com>
2022-03-28 14:43:34 +0530

    Feat: Add Signup Page

--- src/pages/authentication/Signup.jsx
+++ src/pages/authentication/Signup.jsx
@@ -1,73 +1,103 @@
 import "./Authentication.css";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { TextInput, PasswordInput } from "./components/index";
+import { useAuth } from "../../context";
+import { signupService } from "../../services";
+import { checkIfAllInputsAreNotEmpty } from "../../helpers";
+
 
 export function Signup() {
+  const [userData, setUserData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+    passwordsDifferent: false,
+  });
+  const navigate = useNavigate();
+  const { authDispatch } = useAuth();
+
+  const handleSubmitClick = (e) => {
+    e.preventDefault();
+    if (!checkIfAllInputsAreNotEmpty(userData)) {
+      alert("Inputs cannot be Empty");
+    } else if (userData.password !== userData.confirmPassword) {
+      setUserData({ ...userData, passwordsDifferent: true });
+    } else {
+      signupService(userData, authDispatch, navigate);
+    }
+  };
+
   return (
     <>
       <main className="main-container flex flex-center">
         <form className="authentication-container flex flex-column ai-left p-md2 m-xs dark">
           <h1 className="title m-s m-rl0 fs-3 fw-600">Sign up</h1>
-          <div className="input-wrapper m-xxxs m-rl0">
-            <label htmlFor="email">Full Name</label>
-            <input
+          <div className="flex c-gap-1rem">
+            <TextInput
+              labelText="First Name"
+              id="firstName"
+              name="firstName"
+              placeholder="John"
+              userData={userData}
+              setUserData={setUserData}
               type="text"
-              id="name"
-              placeholder="John Doe"
-              className="input p-xxs m-xxs m-rl0 bd-rad-sm"
+            />
+            <TextInput
+              labelText="Last Name"
+              id="lastName"
+              name="lastName"
+              placeholder="Doe"
+              userData={userData}
+              setUserData={setUserData}
+              type="text"
             />
           </div>
-          <div className="input-wrapper m-xxxs m-rl0">
-            <label htmlFor="email">Email</label>
-            <input
-              type="text"
-              id="email"
-              placeholder="johndoe@gmail.com"
-              className="input p-xxs m-xxs m-rl0 bd-rad-sm"
-            />
-            <label className="success-msg m-xxs m-rl0">
-              Success! Email entered is correct.
-            </label>
-          </div>
-          <div className="input-wrapper m-xxxs m-rl0">
-            <label htmlFor="password">Password</label>
-            <div className="input-wrapper bd-rad-sm input-w-btn flex flex-row ai-center m-xxs m-rl0">
-              <input
-                type="text"
-                className="p-xxs input"
-                placeholder="password"
-              />
-              <div className="p-xxs gray-text input-btn">
-                <i className="fa-solid fa-eye" />
-              </div>
-            </div>
-            <label className="error-msg">Wrong Password. Try again.</label>
-          </div>
-          <div className="input-wrapper m-xxs m-rl0">
-            <label className="" htmlFor="password">
-              Confirm Password
-            </label>
-            <div className="input-wrapper bd-rad-sm input-w-btn flex flex-row ai-center m-xxs m-rl0">
-              <input
-                type="text"
-                className="p-xxs input"
-                placeholder="confirm password"
-              />
-              <div className="p-xxs gray-text input-btn">
-                <i className="fa-solid fa-eye" />
-              </div>
-            </div>
-            <label className="error-msg">Passwords do not match.</label>
-          </div>
+          <TextInput
+            labelText="Email"
+            id="email"
+            name="email"
+            placeholder="johndoe@gmail.com"
+            userData={userData}
+            setUserData={setUserData}
+            type="email"
+          />
+
+          <PasswordInput
+            labelText="Password"
+            id="password"
+            name="password"
+            placeholder="password"
+            userData={userData}
+            setUserData={setUserData}
+          />
+
+          <PasswordInput
+            labelText="Confirm Password"
+            id="confirm-password"
+            name="confirmPassword"
+            placeholder="confirm password"
+            userData={userData}
+            setUserData={setUserData}
+          />
+
           <p className="m-xxs m-rl0 gray-text fs-6">
             By signing up, you agree to our terms and conditions.
           </p>
-          <button type="submit" className="btn btn-primary m-xxs m-rl0">
+          <button
+            type="submit"
+            className="btn btn-primary m-xxs m-rl0"
+            onClick={handleSubmitClick}
+          >
             Sign Up
           </button>
-          <p className="m-xxs m-rl0 center-align-text">
-            Already a User?
-            <a className="primary-color-text" href="/pages/Login/Login.html">
+          <p className="m-xxs m-rl0 center-align-text gray-text">
+            Already a User?{" "}
+            <Link to="/login" className="primary-color-text">
               Log in
-            </a>
+            </Link>
           </p>
         </form>
       </main>
