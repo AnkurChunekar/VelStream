4d1cd419d526859ed9de6fa0c1101c2fab65c13f
Ankur Chunekar <89077985+AnkurChunekar@users.noreply.github.com>
2022-03-28 14:47:48 +0530

    feat: Add Signup Service

---
+++ src/services/authServices/signup.service.js
@@ -0,0 +1,38 @@
+import axios from "axios";
+
+export const signupService = async (userData, authDispatch, navigate) => {
+
+    try {
+        const { firstName, lastName, email, password } = userData;
+
+        const response = await axios.post("/api/auth/signup", {
+            firstName,
+            lastName,
+            email,
+            password,
+        });
+
+        switch (response.status) {
+            case 201:
+                localStorage.setItem("token", response.data.encodedToken);
+                authDispatch({
+                    type: "SIGN_UP",
+                    payload: {
+                        user: response.data.createdUser,
+                        token: response.data.encodedToken,
+                    },
+                });
+                alert("Signup Successfull!");
+                navigate("/");
+                break;
+            case 422:
+                throw new Error("User already exists");
+            case 500:
+                throw new Error("Server Error");
+            default:
+                throw new Error("Unknown Error Occured.");
+        }
+    } catch (error) {
+        alert("Error Occured, Please Try Again.", error)
+    }
+}
