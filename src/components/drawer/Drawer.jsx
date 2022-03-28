6f8659c60475edeb3cc6d44447e7c313494148f0
Ankur Chunekar <89077985+AnkurChunekar@users.noreply.github.com>
2022-03-28 15:58:19 +0530

    Refactor: Remove empty strings provided by prettier

--- src/components/drawer/Drawer.jsx
+++ src/components/drawer/Drawer.jsx
@@ -3,58 +3,54 @@ import { Link } from "react-router-dom";
 import { useAuth } from "../../context";
 
 export function Drawer() {
-  const { authState: { user } } = useAuth();
+  const {
+    authState: { user },
+  } = useAuth();
 
   return (
     <div className="nav-drawer dark fs-5">
       <div className="drawer-item">
         <span>
-          {" "}
-          <i className="fa-solid fa-house"></i>{" "}
+          <i className="fa-solid fa-house"></i>
         </span>
         <span> Home </span>
       </div>
       <Link to="/">
         <div className="drawer-item">
           <span>
-            {" "}
-            <i className="fa-regular fa-compass"></i>{" "}
+            <i className="fa-regular fa-compass"></i>
           </span>
           <span> Explore </span>
         </div>
       </Link>
-      <Link to={ user ? "/playlist" : "/login" }>
+      <Link to={user ? "/playlist" : "/login"}>
         <div className="drawer-item">
           <span>
-            {" "}
-            <i className="fa-solid fa-list-check"></i>{" "}
+            <i className="fa-solid fa-list-check"></i>
           </span>
           <span> Playlists </span>
         </div>
       </Link>
-      <Link to={ user ? "/watchlater" : "/login" }>
+      <Link to={user ? "/watchlater" : "/login"}>
         <div className="drawer-item">
           <span>
-            {" "}
-            <i className="fa-regular fa-clock"></i>{" "}
+            <i className="fa-regular fa-clock"></i>
           </span>
           <span> Watch Later </span>
         </div>
       </Link>
-      <Link to={ user ? "/liked" : "/login" }>
+      <Link to={user ? "/liked" : "/login"}>
         <div className="drawer-item">
           <span>
-            {" "}
-            <i className="fa-solid fa-thumbs-up"></i>{" "}
+            <i className="fa-solid fa-thumbs-up"></i>
           </span>
           <span> Liked </span>
         </div>
       </Link>
-      <Link to={ user ? "/history" : "/login" }>
+      <Link to={user ? "/history" : "/login"}>
         <div className="drawer-item">
           <span>
-            {" "}
-            <i className="fa-solid fa-clock-rotate-left"></i>{" "}
+            <i className="fa-solid fa-clock-rotate-left"></i>
           </span>
           <span> History </span>
         </div>
