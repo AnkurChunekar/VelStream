2d67190b5025e9969a90eae82755ecb26bd64cc0
Ankur Chunekar <89077985+AnkurChunekar@users.noreply.github.com>
2022-03-28 14:45:42 +0530

    Refactor: replace anchor tags with Links

--- src/components/drawer/Drawer.jsx
+++ src/components/drawer/Drawer.jsx
@@ -1,41 +1,64 @@
 import "./Drawer.css";
 import { Link } from "react-router-dom";
+import { useAuth } from "../../context";
 
-export function Drawer(){
-    return <div className="nav-drawer dark fs-5">
+export function Drawer() {
+  const { authState: { user } } = useAuth();
+
+  return (
+    <div className="nav-drawer dark fs-5">
+      <div className="drawer-item">
+        <span>
+          {" "}
+          <i className="fa-solid fa-house"></i>{" "}
+        </span>
+        <span> Home </span>
+      </div>
+      <Link to="/">
         <div className="drawer-item">
-            <span > <i className="fa-solid fa-house"></i> </span>
-            <span> Home </span>
+          <span>
+            {" "}
+            <i className="fa-regular fa-compass"></i>{" "}
+          </span>
+          <span> Explore </span>
         </div>
-      <Link to="/" >
+      </Link>
+      <Link to={ user ? "/playlist" : "/login" }>
         <div className="drawer-item">
-            <span > <i className="fa-regular fa-compass"></i> </span>
-            <span> Explore </span>
+          <span>
+            {" "}
+            <i className="fa-solid fa-list-check"></i>{" "}
+          </span>
+          <span> Playlists </span>
         </div>
-        </Link>
-        <Link to="/playlist" >
+      </Link>
+      <Link to={ user ? "/watchlater" : "/login" }>
         <div className="drawer-item">
-            <span > <i className="fa-solid fa-list-check"></i> </span>
-            <span> Playlists </span>
+          <span>
+            {" "}
+            <i className="fa-regular fa-clock"></i>{" "}
+          </span>
+          <span> Watch Later </span>
         </div>
-        </Link>
-        <Link to="/watchlater" >
+      </Link>
+      <Link to={ user ? "/liked" : "/login" }>
         <div className="drawer-item">
-            <span > <i className="fa-regular fa-clock"></i> </span>
-            <span> Watch Later </span>
+          <span>
+            {" "}
+            <i className="fa-solid fa-thumbs-up"></i>{" "}
+          </span>
+          <span> Liked </span>
         </div>
-        </Link>
-        <Link to="/liked" >
+      </Link>
+      <Link to={ user ? "/history" : "/login" }>
         <div className="drawer-item">
-            <span > <i className="fa-solid fa-thumbs-up"></i> </span>
-            <span> Liked </span>
+          <span>
+            {" "}
+            <i className="fa-solid fa-clock-rotate-left"></i>{" "}
+          </span>
+          <span> History </span>
         </div>
-        </Link>
-        <Link to="/history" >
-        <div className="drawer-item">
-            <span > <i className="fa-solid fa-clock-rotate-left"></i> </span>
-            <span> History </span>
-        </div>
-        </Link>
+      </Link>
     </div>
+  );
 }
