b4de2ea9d6344a39be676165d39a82f56cb60227
Ankur Chunekar <89077985+AnkurChunekar@users.noreply.github.com>
2022-03-31 14:59:39 +0530

    Feat: render videos on watch later page

--- src/pages/watchLater/WatchLater.jsx
+++ src/pages/watchLater/WatchLater.jsx
@@ -1,13 +1,38 @@
 import "./WatchLater.css";
-import { ContentSidebar, Drawer } from "../../components";
+import { ContentSidebar, Drawer, HorizontalCard } from "../../components";
+import { useWatchLater } from "../../context";
+import { Link } from "react-router-dom";
+import { Fragment } from "react";
 
 export function WatchLater() {
+  const {
+    watchLaterState: { watchLaterData },
+  } = useWatchLater();
+
   return (
     <main className="flex page-container">
       <Drawer />
       <div className="flex w-100pc">
-        <ContentSidebar pageTitle={"Watch Later"} />
-        <div className="flex flex-column h-card-container"></div>
+        <ContentSidebar
+          pageTitle={"Watch Later"}
+          sidebarBanner={
+            watchLaterData.length > 0 ? watchLaterData[0].videoThumbnail : undefined
+          }
+          videoCount={watchLaterData.length}
+        />
+        <div className="flex flex-column h-card-container">
+          {watchLaterData.length > 0 ? (
+            watchLaterData.map((video) => (
+              <Fragment key={video._id}>
+                <HorizontalCard video={video} />
+              </Fragment>
+            ))
+          ) : (
+            <Link to="/" className="btn btn-primary m-xs">
+              Explore More
+            </Link>
+          )}
+        </div>
       </div>
     </main>
   );
