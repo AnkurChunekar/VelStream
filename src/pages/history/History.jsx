bd565edd149534183048271ff0257b721a9d2955
Ankur Chunekar <89077985+AnkurChunekar@users.noreply.github.com>
2022-04-01 19:16:20 +0530

    Feat: render videos on history page

--- src/pages/history/History.jsx
+++ src/pages/history/History.jsx
@@ -1,12 +1,30 @@
-import { ContentSidebar, Drawer } from "../../components";
+import { ContentSidebar, Drawer, HorizontalCard } from "../../components";
+import { Fragment } from "react";
+import { useHistory } from "../../context";
 
 export function History() {
+  const {
+    historyState: { historyData },
+  } = useHistory();
+
   return (
     <main className="flex page-container">
       <Drawer />
       <div className="flex w-100pc">
-        <ContentSidebar pageTitle={"History"} />
-        <div className="flex flex-column h-card-container"></div>
+        <ContentSidebar
+          pageTitle={"History"}
+          sidebarBanner={
+            historyData.length > 0 ? historyData[0].videoThumbnail : undefined
+          }
+          videoCount={historyData.length}
+        />
+        <div className="flex flex-column h-card-container">
+          {historyData.map((video) => (
+            <Fragment key={video._id}>
+              <HorizontalCard video={video} />
+            </Fragment>
+          ))}
+        </div>
       </div>
     </main>
   );
