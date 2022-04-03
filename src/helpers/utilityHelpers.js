49aba9ea2e0047671f6e6e9e674a2a51d9f515a4
Ankur Chunekar <89077985+AnkurChunekar@users.noreply.github.com>
2022-04-03 14:50:14 +0530

    Feat: add helper function to capitalize 1st word of string

--- src/helpers/utilityHelpers.js
+++ src/helpers/utilityHelpers.js
@@ -9,3 +9,5 @@ export const checkIfAllInputsAreNotEmpty = (userData) => {
 };
 
 export const checkIfItemInArrOfObj = (array, callback) => array.some(callback);
+
+export const capitalizeFirstWordOfString = str =>  str.slice(0, 1).toUpperCase() + str.slice(1, str.length);
