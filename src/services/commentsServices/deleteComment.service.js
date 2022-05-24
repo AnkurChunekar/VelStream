a2c593455a037351bb794fd96301ac3699de61cb
Ankur Chunekar <89077985+AnkurChunekar@users.noreply.github.com>
2022-05-24 13:54:10 +0530

    Feat: delete comment service

---
+++ src/services/commentsServices/deleteComment.service.js
@@ -0,0 +1,28 @@
+import { toast } from "react-toastify";
+import axios from "axios";
+
+export const deleteCommentService = async (
+  token,
+  videoId,
+  comment,
+  commentsDispatch
+) => {
+  try {
+    const response = await axios.post(
+      `/api/user/comments/delete/${videoId}`,
+      { comment },
+      { headers: { authorization: token } }
+    );
+
+    if (response.status === 200) {
+      commentsDispatch({
+        type: "UPDATE_COMMENTS",
+        payload: response.data.comments,
+      });
+      toast.success("Comment Deleted successfully");
+    }
+  } catch (error) {
+    console.error(error);
+    toast.error("Error Occured, please try again!");
+  }
+};
